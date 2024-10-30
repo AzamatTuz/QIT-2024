9a7936b0b4858a56b1a97c5f94bd6f04b84a819c
Azamat <tuzelbajazamat361@gmail.com>
2024-10-30 18:12:06 +0500

    hw 30.10

---
+++ Home Work/30.10/script.js
@@ -0,0 +1,15 @@
+let modelName = "Matiz";
+
+let color = "green";
+
+let wheelsCount = 4;
+
+let isOffRoad = "Joq"
+
+let rating = 4.5;
+
+console.log("Модель атауы", modelName);
+console.log("Түсі", color);
+console.log("Дөңгелек саны", wheelsCount);
+console.log("Жолсыз аймақпен (бездорожье) жүруге арналған ба", isOffRoad);
+console.log("Рейтингі", rating);
