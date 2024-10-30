e5d2167267ec09a6ab18067055d32f8796c96342
Azamat <tuzelbajazamat361@gmail.com>
2024-10-30 12:36:44 +0500

    Task

---
+++ Examples/30.10/script.js
@@ -0,0 +1,12 @@
+var name = "Aty"; //ozgeredi string
+
+const ID = 1212132323; //ozgermeidi number
+
+console.log(ID);
+
+let season = 'Fall';
+                                    //Boolean
+console.log(season === 'Summer');
+
+let grades = [3, 4, 5, 5, 5, 4]; //Array
+
