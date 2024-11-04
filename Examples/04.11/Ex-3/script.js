eff022a4310ad0298dcdb7a4e2b59f29a2958ece
Azamat <tuzelbajazamat361@gmail.com>
2024-11-04 17:59:27 +0500

    р

---
+++ Examples/04.11/Ex-3/script.js
@@ -0,0 +1,6 @@
+var a = 4;
+var b = 4;
+
+var p = (a + b) * 2;
+
+console.log((a == b) && (p >= 30 && p <= 40));
---
+++ Examples/04.11/Ex-4/script.js
@@ -0,0 +1,30 @@
+// let a = 81;
+// let b = 62;
+
+// console.log(a % 2 == 0 && b % 5 == 0);
+
+// let temp1 = 24;
+// let temp2 = 45;
+
+// console.log((temp1 >= 15 && temp1 <= 25) && temp2 > 30);
+
+// let sale = 10;
+// let price = 60;
+
+// price -= sale;
+
+// console.log(price >= 50 && sale <= 20);
+
+// let score1 = 10;
+// let score2 = 80;
+// let score3 = 80;
+
+// let averageScore = (score1 + score2 + score3) / 3;
+
+// console.log(averageScore >= 70 && averageScore <= 90);
+// console.log(score1 > 75 || score2 > 75 || score3 > 75);
+// console.log(score1 + score2 > score3);
+
+
+
+
