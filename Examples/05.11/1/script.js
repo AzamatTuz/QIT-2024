61c38b5752127c10091de9d34ecf6d8ae5b59c12
Azamat <tuzelbajazamat361@gmail.com>
2024-11-05 11:37:48 +0500

    cw

---
+++ Examples/05.11/1/script.js
@@ -0,0 +1,6 @@
+
+let name = prompt("Enter your name")
+let age = prompt("Enter your age")
+let email = prompt("Enter your email")
+
+alert("Hello " + name + "\n" + "You are " + age + " years old" + "\n" + "Your email is " + email)
---
+++ Examples/05.11/2/script.js
@@ -0,0 +1,7 @@
+let num = prompt("Enter your number")
+
+if (num % 2 == 0) {
+    alert("Your number " + num + " even")
+} else {
+    alert("Your number " + num + " odd")
+}
