5d0e191a380d94974917e2c1304988f2dac892c8
Azamat <tuzelbajazamat361@gmail.com>
2024-11-06 12:49:57 +0500

    ы

--- Examples/06.11/3/script.js
+++ Examples/06.11/3/script.js
@@ -3,44 +3,44 @@ var isOrder = prompt("Бургер тапсырыс беріңіз" + "\n" + "А
 var meet = ""
 
 if (isOrder == "а" || isOrder == "А") {
-    var meet = "Куринный";
+    meet += "Куринный";
 } else if (isOrder == "б" || isOrder == "Б") {
-    var meet = "Говяжий";
+    meet += "Говяжий";
 } else if (isOrder == "с" || isOrder == "С") {
-    var meet = "Етсіз";
+    meet += "Етсіз";
 }
 
 var isOrder2 = prompt("Сусын тапсырыс беріңіз" + "\n" + "А)Кола" + "\n" + "Б)Спрайт" + "\n" + "С)Фанта");
 var water = ""
 
 if (isOrder2 == "а" || isOrder == "А") {
-    var water = "Кола";
+    water += "Кола";
 } else if (isOrder == "б" || isOrder == "Б") {
-    var water = "Спрайт";
+    water += "Спрайт";
 } else if (isOrder == "с" || isOrder == "С") {
-    var water = "Фанта";
+    water += "Фанта";
 }
 
 var isOrder3 = prompt("Фри тапсырыс беріңіз" + "\n" + "А)Кіш" + "\n" + "Б)Орт" + "\n" + "С)Үлк");
 var fri = ""
 
 if (isOrder3 == "а" || isOrder == "А") {
-    var fri = "Кіш";
+    fri += "Кіш";
 } else if (isOrder == "б" || isOrder == "Б") {
-    var fri = "Орт";
+    fri += "Орт";
 } else if (isOrder == "с" || isOrder == "С") {
-    var fri = "Үлк";
+    fri += "Үлк";
 }
 
 var isOrder = prompt("Соус таңдаңыз" + "\n" + "А)BBQ" + "\n" + "Б)Cheese" + "\n" + "С)Ketchup");
 var souce = ""
 
 if (isOrder == "а" || isOrder == "А") {
-    var souce = "BBQ";
+    souce += "BBQ";
 } else if (isOrder == "б" || isOrder == "Б") {
-    var souce = "Cheese";
+    souce += "Cheese";
 } else if (isOrder == "с" || isOrder == "С") {
-    var souce = "Ketchup";
+    souce += "Ketchup";
 }
 
 alert("Бургер " + meet + "\n" + "Напиток " + water + "\n" + "Фри " + fri + "\n" + "Соус " + souce + "\n" + "Клиент аты " + herName)
