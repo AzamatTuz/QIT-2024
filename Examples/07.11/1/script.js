54bf0e148c6d61048a5d940b9747166e99c3c8cc
Azamat <tuzelbajazamat361@gmail.com>
2024-11-07 11:27:18 +0500

    1

---
+++ Examples/07.11/1/script.js
@@ -0,0 +1,16 @@
+let option = prompt("1-ден 3-ке дейінгі сан таңдаңыз:");
+
+switch (option) {
+    case '1':
+        alert("Сіз 1 таңдадыңыз");
+        break;
+    case '2':
+        alert("Сіз 2 таңдадыңыз");
+        break;
+    case '3':
+        alert("Сіз 3 таңдадыңыз");
+        break;
+    default:
+        alert("Қате вариант");
+        break;
+}
