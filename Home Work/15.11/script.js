a13d44d20b7995c793e5b4d8832d81bdeb5aa2d0
Azamat <tuzelbajazamat361@gmail.com>
2024-11-17 18:45:06 +0500

    Task

--- Home Work/15.11/script.js
+++ Home Work/15.11/script.js
@@ -1,7 +1,26 @@
-for ( let i = 1; i <= 5; i++) {
-    var quest = prompt('Дұрыс парольды енгіз');
-    if (quest == amjilt_the_best) {
-        alert('ok')
+// for ( let i = 1; i <= 5; i++) {
+//     var quest = prompt('Дұрыс парольды енгіз');
+//     if (quest == 'amjilt_the_best') {
+//         alert('ok')
+//         break;
+//     }
+// }
+
+
+let i = 1;
+
+
+while (i <= 5) {
+    let password = prompt('Your password');
+    if (password == 'amjilt_the_best') {
+        alert('ok');
         break;
     }
+
+    if (i == 5) {
+        alert('Too many attempts');
+        break;
+    }
+
+    i++;
 }
