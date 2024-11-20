48c5040304775ffe1ef58c1609162c3efc4bd17e
Azamat <tuzelbajazamat361@gmail.com>
2024-11-20 11:35:28 +0500

    Task.

---
+++ Examples/20.11/1/script.js
@@ -0,0 +1,15 @@
+let money = +prompt('All money');
+let kurs = prompt('Dollar or Tenge');
+
+function conv(a, b) {
+    switch (b) {
+        case "Dollar":
+            return a * 494.4 + " Tenge";
+    case "Tenge":
+            return a / 494.4 + " Dollar";
+        default:
+            return "Dyrys engiz"
+    }
+}
+
+alert(conv(money, kurs))
