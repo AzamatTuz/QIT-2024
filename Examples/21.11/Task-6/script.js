30ea99c8dfdc6051337f08c364c7811084b7b2e8
Azamat <tuzelbajazamat361@gmail.com>
2024-11-24 17:50:23 +0500

    Task 1000

---
+++ Examples/21.11/Task-6/script.js
@@ -0,0 +1,8 @@
+let num = +prompt('San engiz');
+
+for (let i = num; i >= 1; i--) {
+        console.log(i);
+}
+
+
+
---
+++ Examples/21.11/Task-7/script.js
@@ -0,0 +1,47 @@
+function all() {
+    let all = 0;
+    let cny = +prompt("Neshe CNY bar ?");
+    let usd = +prompt('Neshe USD bar ?');
+    let kzt = +prompt('Neshe KZT bar ?');
+
+    dollar = cny * 0.14;
+    all += dollar;
+    dollar = kzt * 0.0020;
+    all += dollar;
+    all += usd;
+
+    alert('Sizde ' + all + ' USD bar');
+
+    while (true) {
+        let order = prompt('Zakaz ber' + '\n' + '1)Pizza 20 USD' + '\n' + '2)Sushi 10 USD' + '\n' + '3)Coffee 5 USD' + '\n' + '4)Fri 1 USD');
+
+        if (order == '1' && all >= 20) {
+            all -= 20;
+            alert('Sizde ' + all + ' USD kaldy');
+        } else if (order == '1' && all < 20) {
+            alert('Sizde karajat jetkiliksiz');
+            break;
+        } else if (order == '2' && all >= 10) {
+            all -= 10;
+            alert('Sizde ' + all + ' USD kaldy');
+        } else if (order == '2' && all < 10) {
+            alert('Sizde karajat jetkiliksiz');
+            break;
+        } else if (order == '3' && all >= 5) {
+            all -= 5;
+            alert('Sizde ' + all + ' USD kaldy');
+        } else if (order == '3' && all < 5) {
+            alert('Sizde karajat jetkiliksiz');
+            break;
+        } else if (order == '4' && all >= 1) {
+            all -= 1;
+            alert('Sizde ' + all + ' USD kaldy');
+        } else if (order == '4' && all < 1) {
+            alert('Sizde karajat jetkiliksiz');
+            break;
+        }
+    }
+
+}
+
+all()
