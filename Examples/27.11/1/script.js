bb261f45f2e41b6ebcedc3857f53b2221821e020
Azamat <tuzelbajazamat361@gmail.com>
2024-11-27 11:44:25 +0500

    ++

---
+++ Examples/27.11/1/script.js
@@ -0,0 +1,26 @@
+let cities = ['Almaty', 'Atyrau', 'Aktau', 'Shymkent', 'Astana'];
+// cities.push('Kyzylorda', 'Semei');
+// cities.unshift('London');
+
+// console.log(cities);
+
+// let a = cities.pop();
+// console.log(cities);
+
+// let b = cities.shift();
+// console.log(cities);
+
+// console.log(cities[2]);
+
+cities[3] = 'Kokshetau';
+cities[cities.length - 1] = 'Oral'
+
+let arr = cities;
+arr[2] = 'London'
+
+console.log(cities);
+console.log(arr);
+
+
+
+
