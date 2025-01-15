9747c68dcebe669029dc1e3a67d4293e284feef0
Azamat <tuzelbajazamat361@gmail.com>
2025-01-15 13:29:28 +0500

    Weather API

---
+++ Examples/15.01.25/script.js
@@ -0,0 +1,36 @@
+const apiKey = 'fb2cff02b71168e9f0c68fd3f83d168a';
+const weatherBtn = document.getElementById('weatherBtn');
+const cont = document.getElementById('cont')
+
+weatherBtn.addEventListener('click', fetchData)
+
+function fetchData() {
+    const city = document.getElementById('weatherInput');
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=metric;`
+    city.value = ""
+    fetch(api)
+        .then(response => response.json())
+        .then(data => {
+            const cityName = data.name
+            const weather = data.main.temp
+            const weatherDescription = data.weather[0].description
+            const weatherText = document.createElement('h4');
+            const cityNameText = document.createElement('h1');
+            const weatherDescriptionText = document.createElement('h4');
+            const div = document.createElement('div');
+            div.classList.add('weather')
+
+            cityNameText.textContent = cityName
+            weatherText.textContent ='Temperature: ' + weather + ' K'
+            weatherDescriptionText.textContent ='Weather: ' + weatherDescription
+
+            div.appendChild(cityNameText)
+            div.appendChild(weatherText)
+            div.appendChild(weatherDescriptionText)
+
+            cont.appendChild(div)
+            
+        })
+
+        .catch(error => console.error('Kate oryn aldy', error));
+}
