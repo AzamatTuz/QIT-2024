f60e5c26042a15100cd0b220c3dab5fafd054e5b
Azamat <tuzelbajazamat361@gmail.com>
2025-01-28 12:10:07 +0500

    LocalStorage

---
+++ Examples/28.01.25/2/script.js
@@ -0,0 +1,26 @@
+const nameInput = document.getElementById('nameInput');
+const ageInput = document.getElementById('ageInput');
+const saveButton = document.getElementById('saveButton');
+const getButton = document.getElementById('getButton');
+const clearButton = document.getElementById('clearButton');
+
+saveButton.addEventListener('click', () => {
+    localStorage.setItem('name', nameInput.value);
+    localStorage.setItem('age', ageInput.value);
+
+    alert('Data is saved');
+});
+
+getButton.addEventListener('click', () => {
+    const name = localStorage.getItem('name');
+    const age = localStorage.getItem('age');
+
+    alert(`Name: ${name} age: ${age}`)
+    
+})
+
+clearButton.addEventListener('click', () => {
+    localStorage.clear();
+
+    alert('Clear')
+})
